b37b1e0a624ffda5fc94c00b9e196eeb215f3fc0
joshuanieh <b07502027@ntu.edu.tw>
2022-08-20 18:48:04 +0800

    finish

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import math
 from qiskit.circuit.library import MCXGate
 from qiskit import QuantumCircuit
 from qiskit.circuit import QuantumRegister, ClassicalRegister
-#step 1 2: Construct original truth table plus the preservation bits
+
+#Step 1 2: Construct original truth table plus the preservation bits
 clasical_boolean_logic = []
 inputs = ''
 print("Please enter your clasical boolean logics, q to return: (ex, s (output) = ab' + a'b (input))")
@@ -84,12 +85,13 @@ else:
 	current_truth_table = pd.DataFrame(values,columns=(input_variable_list + [" "] + [" "] + output_variable_list[:]))
 print("\nClassical truth table:")
 print(current_truth_table.to_string(index=False))
+print()
 
-#step 3: Eliminate duplicate output
+#Step 3: Eliminate duplicate output
 output_values = [tuple([int(evaluation(dict([(input_variable_list[i], x[i]) for i in range(len(input_variable_list))]), func)) for func in function_string] + [int(x[input_variable_list.index(variable)]) for variable in p]) for x in product([False, True], repeat=len(input_variable_list))]
 item_count_dictionary = dict([(item, count) for item, count in collections.Counter(output_values).items() if count > 1])
-z = max([count for item, count in collections.Counter(output_values).items()])
-z = int(math.ceil(math.log(z, 2)))
+max_output_repetition_count = max([count for item, count in collections.Counter(output_values).items()])
+z = int(math.ceil(math.log(max_output_repetition_count, 2)))
 
 def decimal_to_binary_string(decimal, z):
 	binary = ""
@@ -111,12 +113,14 @@ if z > 0:
 		else:
 			d.append([0 for k in range(z)])
 	values = [[int(i) for i in x] + [" "] + [" "] + list(output_values[j]) + d[j] for j, x in enumerate(product([False, True], repeat=len(input_variable_list)))]
-output_variable_list += [f'd{i}' for i in range(z)]
+additional_output_for_eliminating_repetition = [f'd{i}' for i in range(z)]
+output_variable_list += additional_output_for_eliminating_repetition
 
 # current_truth_table = pd.DataFrame(values,columns=(input_variable_list + [" "] + [" "] + output_variable_list))
-# print("\nTruth table:")
+# print("current truth table:")
 # print(current_truth_table.to_string(index=False))
-t = max(len(input_variable_list), len(output_variable_list))
+# print()
+
 new_output_values = [[] for i in range(len(values))]
 if d != []:
 	for i in range(len(values)):
@@ -124,43 +128,48 @@ if d != []:
 else:
 	for i in range(len(values)):
 		new_output_values[i] = list(output_values[i])
-# print("n: ", new_output_values)
+# print("new output values:", new_output_values)
 
-#step 4: Pad some bits in ouput or input to t and construct the whole transformation table
+#Step 4: Pad some bits in ouput or input to t and construct the whole transformation table
+##Handling input values
+t = max(len(input_variable_list), len(output_variable_list))
 input_values = [[int(k) for k in decimal_to_binary_string(i, t)] for i in range(2**t)]
 l = len(input_variable_list)
-for i in range(t - l):
-	input_variable_list.insert(0, f'x{t - l - i - 1}')
+if t - l:
+	additional_input_for_permutation = [f'x{i}' for i in range(t - l)]
+	input_variable_list = additional_input_for_permutation + input_variable_list
 # print(input_values)
-output_values = new_output_values[:]
+
+##Handling output values
+# output_values = new_output_values[:]
 output_values = [new_output_values[i] + [0 for j in range(t - len(output_variable_list))] for i in range(len(new_output_values))]
+l = len(output_variable_list)
+if t - l:
+	additional_output_for_permutation = [f'a{i}' for i in range(t - l)]
+	output_variable_list = output_variable_list + additional_output_for_permutation
 # print(output_values)
-
-# print(input_variable_list)
-
 output_slots = [0 for i in range(2**t)]
 for i in output_values:
 	# print(i)
 	output_slots[int("".join([f'{j}' for j in i]), 2)] = 1
 # print(output_slots)
 # print(output_values)
-current_pos = 0
+counter = 0
 while len(output_values) < 2**t:
-	while output_slots[current_pos] == 1:
-		# print(current_pos)
-		current_pos += 1
-	# print("---")
-	output_values += [[int(i) for i in decimal_to_binary_string(current_pos, t)]]
-	output_slots[current_pos] = 1
+	while output_slots[counter] == 1:
+		# print(counter)
+		counter += 1
+	output_values += [[int(i) for i in decimal_to_binary_string(counter, t)]]
+	counter += 1
 
 # print(output_values) 
 values = [input_values[i] + [" "] + [" "] + output_values[i] for i in range(2**t)]
 # print(values)
-current_truth_table = pd.DataFrame(values,columns=(input_variable_list + [" "] + [" "] + output_variable_list))
-print("\nTransformation table:")
-print(current_truth_table.to_string(index=False))
+transformation_table = pd.DataFrame(values,columns=(input_variable_list + [" "] + [" "] + output_variable_list))
+print("Transformation table:")
+print(transformation_table.to_string(index=False))
 
-#step 5: Permutation and cycle
+#Step 5: Permutation and cycle
 permutation = dict([(int("".join([f'{j}' for j in input_values[i]]), 2), int("".join([f'{j}' for j in output_values[i]]), 2)) for i in range(2**t)])
 # print(permutation)
 cycles = []
@@ -173,9 +182,10 @@ for i in range(2**t):
 		k = permutation.pop(k)
 	if len(l) >= 2:
 		cycles.append(l)
+		
 new_cycles = []
 for cycle in cycles:
-	new_cycle = []
+	# new_cycle = []
 	for i, j in enumerate(cycle):
 		if i == len(cycle) - 1:
 			break
@@ -183,7 +193,7 @@ for cycle in cycles:
 	# new_cycles.append(new_cycle)
 # print(new_cycles)
 
-#step 6: T(S, R, I) gate
+#Step 6: T(S, R, I) gate
 cycles = new_cycles
 new_cycles = []
 # print(cycles)
@@ -212,7 +222,7 @@ for cycle in cycles:
 		new_cycles += tmp_cycles[-2::-1]
 # print(new_cycles)
 
-#step 7: Draw
+#Step 7: Draw
 input_registers = []
 for i in range(t):
 	input_registers.append(QuantumRegister(1, name=input_variable_list[t - i - 1]))
