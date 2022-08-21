e03a56dd8169fc28bbb10ca7248f725bd0eb6916
joshuanieh <b07502027@ntu.edu.tw>
2022-08-21 09:23:34 +0800

    delete test file

--- test.py
+++
@@ -1,8 +0,0 @@
-from qiskit.circuit.library import MCXGate
-gate = MCXGate(4, ctrl_state = '1000')
-
-from qiskit import QuantumCircuit
-circuit = QuantumCircuit(5)
-circuit.append(gate, [0, 1, 2, 4, 3])
-circuit.draw(output='mpl',filename='circuit.png')
-print(circuit)
